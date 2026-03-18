951109447dfba1e6267ed55b533ab5ed869958fa
Nikoloz <nikolozsarauli@gmail.com>
2026-03-18 04:03:34 +0400

    A mini terminal game

--- study1/study1.c
+++ study1/study1.c
@@ -10,176 +10,206 @@
 //------------------------------------------------------------------/
 // Asset Section
 
-typedef struct{ //Score struct
+typedef struct{ //Player struct
 
-    int currentScore; // Current Score
-    int goalScore;    // Goal Score
-    
-}   Score;
+    char name[16];//Player name
+    int score;//Player score
+    int xx; //Player X coords
+    int yy; //Player Y coords
 
-typedef struct { //Coordinates struct
-
-    int xx; //Player X coordinate
-    int yy; //Player Y coordinate
-
-} Coords;
-
-
-typedef struct{
-    char name[16]; // Player name
-    int losses; // Amount of Loses
-    int wins; // Amount of Wins
-
-}   Player;
-
+} Player;
 //------------------------------------------------------------------/
 // Global Variable Section
 int x; //X coordinate for drawMap()
 int y; //Y coordinate for drawMap()
 int goal; //Goal score integer
-char hero = '@';
-char villain = 'X';
-char action;
+char hero = '@'; //Our hero character
+char villain = 'X'; //Enemy
+char action; //Input key (moving)
+Player player; //Declaring player or sum, idk yet
+
 ////------------------------------------------------------------------/
 // Text Section
 
 //FUNCTION PROTOTYPES - functions explained below!
-void drawMap(int randomX, int randomY); 
-void spawnEnemy(int randomX, int randomY);
-void spawnPlayer(int randomX, int randomY);
-void spawnCoin();
-void healthStatus();
-void spawnStuff();
-void moving(int randomX, int randomY);
+void drawMap(int randomX, int randomY); // Draws map
+//void spawnPlayer(int randomX, int randomY); // Spawns Player
+void moving();  // pretty self explanatory
+void updateMap(int randomX, int randomY); // Map update after action
+void moveUp(); // up
+void moveDown(); // down
+void moveLeft(); // left
+void moveRight(); // right
 
 //MAIN FUNCTION
 int main(){ //Main
-    //Local Variables
-    srand(time(NULL)); // seeding
-                       
-    int randomX = (rand() % 19) + 1;  // Random X position
-    int randomY = (rand() % 9) + 1;   // Random Y position
 
-    Player player;
+    srand(time(NULL)); //seeding random number so it updates per-every change of clock
+
+    int randomX = (rand() % 19) + 1; // random number from 1 to 19
+    int randomY = (rand() % 9) + 1; //random number from 1 to 9
+    //such weird numbers are necessary for character to not spawn outside box
+
+    player.xx = randomX; // player coordinate X is random from 1 to 19
+    player.yy = randomY; // player coordinate Y is random from 1 to 9
+                         // so they won't touch borders
 
     //Asks user to enter Name
     printf("What is player name? \n");
     //User inputs name
-    fgets(player.name, sizeof(player.name), stdin);
+     fgets(player.name, sizeof(player.name), stdin);
     //Getting rid of new line after input
-    player.name[strcspn (player.name, "\n")] = '\0';
+     player.name[strcspn (player.name, "\n")] = '\0';
     
     //Asks user to enter Score Goal
     printf("What is the score goal? \n");
-    scanf("%d", &goal);
+    //check the input 
+     scanf("%d", &goal);
+
     
     printf("\n"); //new line 
     printf("Great! Goodluck\n"); //stupid message for decoration
-    Sleep(100);
-    system("cls");
+    printf("press F to quit game\n"); //additional info
+    Sleep(500); //wait 0.2 seconds or sum like that 
+    system("cls"); //clear screen
+    drawMap(randomX, randomY); //draw map with character chosen randomly
 
-    moving(randomX, randomY);
+    while (1){ //while game runs
 
-do{
-    system("cls");
-    drawMap(randomX, randomY); // Draw a map, what a suprise right
-}while(action = getch());
+        action = getch();     // get input
+        moving();             // move based on input
+                                  
+        system("cls"); //clear screen to prepare it for new, freshly updated map
+        updateMap(randomX, randomY); //draw updated map
+        if(action == 'f'){ //if f is pressed
+            break; //break loop
+        }//end of if statement
+    }//end of while loop
 
     return 0; // You'll never guess..
 } //End of Main function
 
 
-
-//FUNCTION
-//void scorecounter(int currentScore, int goalScore){ // Counts score
-
-//} // End of function
-
 // FUNCTION
-void moving(int randomX, int randomY){
-    Coords coords;
+void moving(){ //Function responsible for moving character
 
-    if (action == 'w'){
-        coords.yy--;
-    }else if(action == 'a'){
-        coords.yy++;
-    }else if(action == 's'){
-        coords.xx++;
-    }else if(action == 'd'){
-        coords.xx--;
-    }
-}
+    switch (action) { // action variable, which is updated after every click is 
+        case 'w':     //if W is pressed                    // getting new value
+            moveUp(); //execute moveUp funtion
+            break;    //break loop
+        case 'a':     //if A is pressed
+            moveLeft();//execute moveLeft function
+            break;     //break loop
+        case 's':     //if S is pressed
+            moveDown();//execute moveDown function 
+            break;     //break loop
+        case 'd':     //if D is pressed
+            moveRight();//execute moveRight function
+            break;  //break loop
+    }//end of switch statement
+}//End of function
 
 //FUNCTION
-void drawMap(int randomX, int randomY){ // Function that draws map. since X and Y are global variables, we don't need to include parametres.
+void moveUp(){ // checks if path is free and moves player up 
 
-    Coords coords;
+    if(player.yy - 1 != 0){ // checks
+        player.yy--;        // put player at Y with 1 less index than current
+    }//End of for loop
+}//End of function
+
+//FUNCTION
+void moveDown(){  // checks if path is free and moves player down
+
+    if(player.yy + 1 != 10){ // checks
+        player.yy++;         // put player at Y with 1 more index than current
+    }//End of for loop
+}//End of function
+
+//FUNCTION
+void moveLeft(){ // checks if path is free and moves player left
+
+    if(player.xx - 1 != 0){ // checks
+        player.xx--;        // put player at X with 1 less index than current
+    }//End of for loop
+}//End of function
+
+//FUNCTION
+void moveRight(){ // checks if path is free and moves player right
+ 
+    if(player.xx + 1 != 20){ // checks
+        player.xx++;         // put player at X with 1 more index than current
+    }//End of for loop
+}//End of function
+
+//FUNCTION
+void drawMap(int randomX, int randomY){ // Function that draws map
 
     for(int y = 0; y <= 10; y++){ // incrementing y
         for(int x = 0; x <= 20; x++){ // incrementing x
             if(y == 0 || y == 10){ //if y is the corner
-                printf("#"); // printf border
+                printf("#"); // draw border
                 fflush(stdout); //Show output immediately to not buffer #
-                Sleep(10); // Wait 0.05 seconds
+                Sleep(5); // Wait 0.025 seconds
                 if(x == 20){ // but if its the right corner
                   printf("\n"); //then make a new line
-                }
+                }//End of nested if statement
 
+            }else if(x == player.xx && y == player.yy){ //if it's at pos of hero
+                //spawnPlayer(randomX, randomY);        //then print hero
+                printf("%c", hero);       //prints our character
             }else if(x == 0){ //if the X is upper right corner
                 printf("#"); // make border
                 fflush(stdout); // Show output immediately to not buffer #
-                Sleep(10);     // Wait 0.05 seconds
+                Sleep(5);     // Wait 0.025 seconds
 
             }else if(x == 20){ //if the X is upper left corner
                 printf("#\n"); // make border
                 fflush(stdout); //Show output immediately to not buffer #
-                Sleep(10); // Wait 0.05 seconds
+                Sleep(5); // Wait 0.025 seconds
 
             }else{              //if it's rest of place
-                //spawnPlayer(randomX, randomY);
-                //if(x == randomX && y == randomY){
-                  //  printf("%c", hero);
-                //}else{
-                    printf(" ");
-                //}  // leave it empty.
+                    printf(" "); //Just leave it empty
             } // End of for loop inside nested loop
         } // End of nested loop
     } // End of For loop
 } // End of function
 
-
 //FUNCTION
-void spawnEnemy(int randomX, int randomY){ // Spawns Enemy on Map
-} // End of function
+void updateMap(int randomX, int randomY){ // new map drawing
+    for(int y = 0; y <= 10; y++){ // incrementing y
+        for(int x = 0; x <= 20; x++){ // incrementing x
+            if(y == 0 || y == 10){ //if y is the corner
+                printf("#"); // draw border
+                fflush(stdout); //Show output immediately to not buffer #
+                if(x == 20){ // but if its the right corner
+                  printf("\n"); //then make a new line
+                }//End of nested if statement
 
+            }else if(x == player.xx && y == player.yy){
+                //spawnPlayer(randomX, randomY);
+                printf("%c", hero);
+            }else if(x == 0){ //if the X is upper right corner
+                printf("#"); // make border
+                fflush(stdout); // Show output immediately to not buffer #
 
+            }else if(x == 20){ //if the X is upper left corner
+                printf("#\n"); // make border
+                fflush(stdout); //Show output immediately to not buffer #
+            }else{              //if it's rest of place
+                    printf(" "); //Just leave it empty
+            } // End of for loop inside nested loop
+        } // End of nested loop
+    } // End of For loop
+
+}
 //FUNCTION
-void spawnPlayer(int randomX, int randomY){ // Spawns Player on Map  
-    Coords coords;
-    coords.xx = randomX;
-    coords.yy = randomY;
-
+/*void spawnPlayer(int randomX, int randomY){ // Spawns Player on Map  
     
+    printf("%c", hero);
 
-
-} // End of function 
-
-//FUNCTION
-void spawnCoin(){  // Randomly Spawns Coin
-} // End of function
-
-//FUNCTION
-void healthStatus(){ // Displays Health percentage and updates every frame
-} // End of function
-
-//FUNCTION
-void spawnStuff(){ // Function that randomly spawns some cool usables
-} // End of function
-
+} // End of function*/
 //-------------------------------------------------------------------/
 // Comment Section
 
-
-// This is second time i worked on this a little. added jus a few improvements. 
-
-// I will surely work on this way more but right now it's lowk drilling my brains so i will quit
+// I decided to rewrite whole code cuz it looked like bunch of garbage 
+// and made my head spin.
